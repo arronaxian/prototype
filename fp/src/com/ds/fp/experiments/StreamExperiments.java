1ceef6d627ca026cc9563bb21fd04e70c2a8e9cf
arronaxian <contact@theduquettes.net>
2018-08-07 20:34:53 -0700

    Fibonacci integer stream and test

--- fp/src/com/ds/fp/experiments/StreamExperiments.java
+++ fp/src/com/ds/fp/experiments/StreamExperiments.java
@@ -4,9 +4,11 @@ import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Fibonacci {
+public class StreamExperiments {
     public IntStream fibonacciStream = Stream.iterate(
             new int[]{1, 1},
             fib -> new int[] {fib[1], fib[0] + fib[1]}
     ).mapToInt(fib -> fib[0]);
+
+    public IntStream counterStream = Stream.iterate(0, count -> count + 1 ).mapToInt(count -> count);
 }
--- fp/test/com/ds/fp/experiments/StreamsTest.java
+++ fp/test/com/ds/fp/experiments/StreamsTest.java
@@ -8,15 +8,26 @@ import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class FibonacciTest {
+public class StreamsTest {
     @Test
     public void testFibonacciStream() {
         List<Integer> results = Arrays.asList( 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 );
-        Fibonacci fibby = new Fibonacci();
+        StreamExperiments fibby = new StreamExperiments();
 
         final List<Integer> fibResults = new ArrayList<>();
         fibby.fibonacciStream.limit(10).forEach(fib -> fibResults.add(fib));
 
         assertEquals(results, fibResults);
+    }
+
+    @Test
+    public void testCounterStream() {
+        List<Integer> results = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+
+        final List<Integer> values = new ArrayList<>();
+        StreamExperiments sc = new StreamExperiments();
+        sc.counterStream.limit(10).forEach(count -> values.add(count));
+
+        assertEquals(results, values);
     }
 }
